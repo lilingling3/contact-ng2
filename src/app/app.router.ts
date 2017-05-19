c61391af170da3234b1ad28dd7734d6af3782d27
lilingling <lilingling@win-sky.com.cn>
2017-05-19 17:54:39 +0800

    'ng2'

---
+++ src/app/app.router.ts
@@ -0,0 +1,20 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'app-login',
+    template: `
+    <p>
+      login Works!
+    </p>
+  `,
+    styles: []
+})
+export class $ implements $ {
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+}
